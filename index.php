f14bc84a1cf4d147cf5cdbc4ed193318728666da
Dean Petty <deanpetty@Deans-MacBook-Pro.local>
2017-03-20 16:20:25 -0400

    Updated title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/main.css" type="text/css">
-		<title>Holder Title</title>
+		<title>Monster: Encounter</title>
 	</head>
 	<body>
 		<div id="wrapper">
